8987101767d2dd74b4e02a3d8345a7e40c06ca6d
Sihan Chen <sihanchen0317@outlook.com>
2024-11-15 11:24:21 -0500

    new change

--- R/linear.R
+++ R/linear.R
@@ -22,6 +22,7 @@ linear_regress=function(y,x){
   Y = y
   n = nrow(X)
   p = ncol(X)
+
   #### Estimation: betahat and var(betahat) ####
   betahat = solve(t(X)%*%X)%*%t(X)%*%Y
   Yhat = X%*%betahat
